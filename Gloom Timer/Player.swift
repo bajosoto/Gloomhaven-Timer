c4118b267fa0dcd1ff03ea5b81c51c3fb5ada715
bajosoto <sergio.soto@live.com>
2019-03-14 20:04:37 +0100

    Refactor for modularity

--- Gloom Timer/Player.swift
+++ Gloom Timer/Player.swift
@@ -9,16 +9,24 @@
 import Foundation
 import UIKit
 
+let colors: [String: UIColor] = ["tinkerer": UIColor(red: 90/255, green: 69/255, blue: 50/255, alpha: 1.0),
+                                 "brute": UIColor(red: 30/255, green: 60/255, blue: 100/255, alpha: 1.0),
+                                 "mindthief": UIColor(red: 68/255, green: 88/255, blue: 126/255, alpha: 1.0),
+                                 "doomstalker": UIColor(red: 45/255, green: 105/255, blue: 123/255, alpha: 1.0),
+                                 "unassigned": UIColor(red: 20/255, green: 20/255, blue: 20/255, alpha: 1.0)]
+
 class Player {
     var player_class : String
     var player_number : Int
     var player_initiative : Int = -1
     var player_icon : UIImageView
+    var player_color : UIColor = UIColor(red: 20/255, green: 20/255, blue: 20/255, alpha: 1.0)
     
     init (player_class : String, player_number : Int) {
         self.player_class = player_class
         self.player_number = player_number
         self.player_icon = UIImageView()
         self.player_icon.image = UIImage(named: player_class)
+        self.player_color = colors[player_class]!
     }
 }
--- Gloom Timer/ViewController.swift
+++ Gloom Timer/ViewController.swift
@@ -20,30 +20,29 @@ class ViewController: UIViewController {
     var currInitiativePlayer: Int = -1
     
     var players = [Player(player_class: "brute", player_number: 1),
-                   Player(player_class: "doom", player_number: 2),
-                   Player(player_class: "mind", player_number: 3),
-                   Player(player_class: "tink", player_number: 4)]
+                   Player(player_class: "doomstalker", player_number: 2),
+                   Player(player_class: "mindthief", player_number: 3),
+                   Player(player_class: "tinkerer", player_number: 4)]
     
     var effect: UIVisualEffect!
-    
-    let colors: [String: UIColor] = ["tink": UIColor(red: 90/255, green: 69/255, blue: 50/255, alpha: 1.0),
-                                    "brute": UIColor(red: 30/255, green: 60/255, blue: 100/255, alpha: 1.0),
-                                    "mind": UIColor(red: 68/255, green: 88/255, blue: 126/255, alpha: 1.0),
-                                    "doom": UIColor(red: 45/255, green: 105/255, blue: 123/255, alpha: 1.0)]
     
     @IBOutlet weak var currInitiativeLabel: UILabel!
     @IBOutlet weak var globalTimer: UILabel!
     
-    @IBOutlet weak var tinkererButton: UIButton!
-    @IBOutlet weak var bruteButton: UIButton!
-    @IBOutlet weak var mindthiefButton: UIButton!
-    @IBOutlet weak var doomButton: UIButton!
     
-    @IBOutlet weak var player4ImageView: UIImageView!
+    @IBOutlet weak var playerButton1: UIButton!
+    @IBOutlet weak var playerButton2: UIButton!
+    @IBOutlet weak var playerButton3: UIButton!
+    @IBOutlet weak var playerButton4: UIButton!
+    @IBOutlet var playerButtons: [UIButton]!
+    
     @IBOutlet weak var player1ImageView: UIImageView!
-    @IBOutlet weak var player3ImageView: UIImageView!
     @IBOutlet weak var player2ImageView: UIImageView!
+    @IBOutlet weak var player3ImageView: UIImageView!
+    @IBOutlet weak var player4ImageView: UIImageView!
+    @IBOutlet var playerButtonImages: [UIImageView]!
     
+    @IBOutlet weak var invisiBackButton: UIButton!
     
     @IBOutlet var PopupView: UIView!
     @IBOutlet weak var blurFx: UIVisualEffectView!
@@ -58,18 +57,49 @@ class ViewController: UIViewController {
     @IBOutlet weak var np8: UIButton!
     @IBOutlet weak var np9: UIButton!
     @IBOutlet weak var np0: UIButton!
+    @IBOutlet var npButtons: [UIButton]!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Populate player buttons
+        playerButtons = [playerButton1,
+                         playerButton2,
+                         playerButton3,
+                         playerButton4]
+        
+        // Populate player button images
+        playerButtonImages = [player1ImageView,
+                            player2ImageView,
+                            player3ImageView,
+                            player4ImageView]
+        
+        // Populate numpad buttons
+        npButtons = [np1, np2, np3, np4, np5, np6, np7, np8, np9, np0]
+        
         // Store blur effect
         effect = blurFx.effect
         blurFx.effect = nil
+        
+        // Store start time and init timer
         startTime = Date().timeIntervalSinceReferenceDate - elapsed
         timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(UpdateTimer), userInfo: nil, repeats: true)
-        player4ImageView.transform = tinkererButton.transform.rotated(by: CGFloat(Double.pi))
-        player1ImageView.transform = bruteButton.transform.rotated(by: CGFloat(Double.pi))
+        
+        // Assign image to buttons
+        for (i, imageView) in playerButtonImages.enumerated() {
+            imageView.image = UIImage(named: players[i].player_class)
+        }
+        
+        // Assign colors to buttons
+        for (i, button) in playerButtons.enumerated() {
+            button.backgroundColor = players[i].player_color
+        }
+        
+        // Rotate player 1 and 4 buttons upside down
+        player1ImageView.transform = playerButton1.transform.rotated(by: CGFloat(Double.pi))
+        player4ImageView.transform = playerButton4.transform.rotated(by: CGFloat(Double.pi))
     }
 
     @IBAction func numPadButtonPress(_ sender: UIButton) {
@@ -110,9 +140,17 @@ class ViewController: UIViewController {
         }
     }
     
+    @IBAction func invisBackButton(_ sender: Any) {
+        players[currInitiativePlayer].player_initiative = -1
+        print("Player \(currInitiativePlayer + 1) (\(players[currInitiativePlayer].player_class)) initiative is \(players[currInitiativePlayer].player_initiative)")
+        animateNumPadViewOut()
+    }
+    
     @IBAction func playerButtonPress(_ sender: UIButton) {
         animateNumPadViewIn(player: sender.tag)
         currInitiativePlayer = sender.tag - 1
+        currInitiativeOnes = -1
+        currInitiativeTens = -1
     }
     
     func animateNumPadViewIn(player: Int) {
@@ -127,30 +165,12 @@ class ViewController: UIViewController {
             t_out = t_out.rotated(by: CGFloat(Double.pi))
         }
         
-        var charClass: String
-        switch player {
-        case 1:
-            charClass = "brute"
-        case 2:
-            charClass = "doom"
-        case 3:
-            charClass = "mind"
-        case 4:
-            charClass = "tink"
-        default:
-            charClass = "mind"
-        }
+        self.invisiBackButton.isEnabled = true
         
-        np1.backgroundColor = colors[charClass]
-        np2.backgroundColor = colors[charClass]
-        np3.backgroundColor = colors[charClass]
-        np4.backgroundColor = colors[charClass]
-        np5.backgroundColor = colors[charClass]
-        np6.backgroundColor = colors[charClass]
-        np7.backgroundColor = colors[charClass]
-        np8.backgroundColor = colors[charClass]
-        np9.backgroundColor = colors[charClass]
-        np0.backgroundColor = colors[charClass]
+        // Recolor numpad buttons according to class
+        for np in npButtons {
+            np.backgroundColor = players[player - 1].player_color
+        }
         
         self.view.addSubview(PopupView)
         PopupView.center = self.view.center
@@ -166,6 +186,8 @@ class ViewController: UIViewController {
     func animateNumPadViewOut() {
         var t_out = PopupView.transform
         t_out = t_out.scaledBy(x: 1.3, y: 1.3)
+        
+        self.invisiBackButton.isEnabled = false
         
         UIView.animate(withDuration: 0.2, animations: {
             self.PopupView.transform = t_out
